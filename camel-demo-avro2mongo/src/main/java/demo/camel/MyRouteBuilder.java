b6380273a58c764f25a52f74f605f22cd6baee8f
weimeilin79 <weimielin@gmail.com>
2020-11-03 22:31:20 -0500

    Adding Using Apicurio Registry Serde

--- camel-demo-avro2mongo/src/main/java/demo/camel/MyRouteBuilder.java
+++ camel-demo-avro2mongo/src/main/java/demo/camel/MyRouteBuilder.java
@@ -16,13 +16,16 @@ public class MyRouteBuilder extends RouteBuilder {
     @BindToRegistry("mymongo")
     MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         
+    
     public void configure() {
-        AvroDataFormat format = new AvroDataFormat(Transaction.SCHEMA$);
-         
-        from("kafka:transrec?additionalProperties.apicurio.registry.url={{registryurl}}&additionalProperties.apicurio.registry.avro-datum-provider={{datumprovider}}")
-            .unmarshal(format)
+        String registryconfigAvro="?additionalProperties.apicurio.registry.url={{registryurl}}";
+        registryconfigAvro += "&additionalProperties.apicurio.registry.avro-datum-provider={{datumprovider}}";
+        registryconfigAvro += "&additionalProperties.apicurio.registry.global-id={{globalid}}";
+        //AvroDataFormat format = new AvroDataFormat(Transaction.SCHEMA$); //We don't need to do it because the serializer in kafka config does it
+        from("kafka:transrec"+registryconfigAvro)
             .log("---->originalmsg ${body} ")
-            .convertBodyTo(String.class)
+            //.unmarshal(format) //We don't need to do it because the serializer in kafka config does it
+            //.convertBodyTo(String.class)
             .to("mongodb:mymongo?database=example&collection=transaction&operation=save")
         ;
         
--- camel-demo-protobuf2avro/src/main/java/demo/camel/MyRouteBuilder.java
+++ camel-demo-protobuf2avro/src/main/java/demo/camel/MyRouteBuilder.java
@@ -10,14 +10,15 @@ public class MyRouteBuilder extends RouteBuilder {
     
     
     public void configure() {
-        AvroDataFormat format = new AvroDataFormat(Transaction.SCHEMA$);
-        String registryconfig="additionalProperties.apicurio.registry.url={{registryurl}}";
-        registryconfig +="&additionalProperties.apicurio.registry.global-id={{datumprovider}}";
-        registryconfig +="&additionalProperties.apicurio.registry.avro-datum-provider={{globalid}}";
+        //AvroDataFormat format = new AvroDataFormat(Transaction.SCHEMA$); //We don't need to do it because the serializer in kafka config does it
+        String registryconfig="&additionalProperties.apicurio.registry.url={{registryurl}}";
+        String registryconfigAvro = registryconfig+"&additionalProperties.apicurio.registry.avro-datum-provider={{datumprovider}}";
+        registryconfigAvro +="&additionalProperties.apicurio.registry.global-id={{globalid}}";
         
         
-        from("kafka:webtrans?groupId=consumergroup&valueDeserializer=org.apache.kafka.common.serialization.ByteBufferDeserializer")
-            .unmarshal().protobuf("demo.camel.TransactionProtos$Transaction")
+        from("kafka:webtrans?groupId=consumergroup&valueDeserializer={{deserializerClass}}"+registryconfig)
+            .marshal().protobuf("demo.camel.TransactionProtos$Transaction")
+            .bean(demo.camel.TransactionProtos.Transaction.class, "parseFrom(${body})")
             .setHeader("originalmsg",simple("${body}"))
             .multicast().parallelProcessing()
             .to("direct:sender","direct:receiver") 
@@ -25,15 +26,15 @@ public class MyRouteBuilder extends RouteBuilder {
 
         from("direct:sender")
             .setBody(method(this,"setValue(   ${header.originalmsg.sender.userid}, ${header.originalmsg.transactionid}, \"SUB\",${header.originalmsg.currency},${header.originalmsg.amt} )"))
-            .marshal(format)
-            .toD("kafka:transrec?brokers=localhost:9092&groupId=mygroup"+registryconfig)
+            //.marshal(format) //We don't need to do it because the serializer in kafka config does it
+            .toD("kafka:transrec?serializerClass={{serializerClass}}"+registryconfigAvro)
         ;
         //&serializerClass=org.apache.kafka.common.serialization.ByteArraySerializer
         
         from("direct:receiver")
             .setBody(method(this,"setValue( ${header.originalmsg.receiverid}, ${header.originalmsg.transactionid}, \"ADD\", ${header.originalmsg.currency}, ${header.originalmsg.amt} )"))
-            .marshal(format)
-            .toD("kafka:transrec?brokers=localhost:9092&groupId=mygroup"+registryconfig)
+            //.marshal(format) //We don't need to do it because the serializer in kafka config does it
+            .toD("kafka:transrec?serializerClass={{serializerClass}}"+registryconfigAvro)
         ;
         
     }
