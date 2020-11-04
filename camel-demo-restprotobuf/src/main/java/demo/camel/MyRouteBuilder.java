e28c5914c2264878c21f164999b336b2c56678e3
weimeilin79 <weimielin@gmail.com>
2020-11-03 21:21:50 -0500

    update using apricurio protobuf serializer

--- camel-demo-restprotobuf/src/main/java/demo/camel/MyRouteBuilder.java
+++ camel-demo-restprotobuf/src/main/java/demo/camel/MyRouteBuilder.java
@@ -4,6 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.protobuf.ProtobufDataFormat;
 import org.apache.camel.model.rest.RestBindingMode;
 
+
 /**
  * A Camel Java DSL Router
  */
@@ -11,22 +12,28 @@ public class MyRouteBuilder extends RouteBuilder {
 
     
     public void configure() {
+
+        String registryconfig="&additionalProperties.apicurio.registry.url={{registryurl}}";
+        registryconfig +="&additionalProperties.apicurio.registry.global-id={{globalid}}";
+        //ProtobufDataFormat format = new ProtobufDataFormat("demo.camel.TransactionProtos$Transaction");
+
         restConfiguration().component("netty-http").host("localhost").port("8081").bindingMode(RestBindingMode.auto);
         
         rest("/").post("transfer").consumes("application/json").to("direct:transfer");
 
         from("direct:transfer")
-        //.setHeader("receiverid",jsonpath("$.receiverid"))
-        //.setHeader("sender",jsonpath("$.sender.userid"))
-        //.log("---->json ${body.class}")
-        //.log("---->header ${headers}")
-        .marshal().protobuf("demo.camel.TransactionProtos$Transaction")
-        .toD("kafka:webtrans?brokers=localhost:9092&groupId=producergroup&serializerClass=org.apache.kafka.common.serialization.ByteBufferSerializer&key=${header.sender}")
         
+        .setHeader("sender",jsonpath("$.sender.userid"))
+        .marshal().protobuf("demo.camel.TransactionProtos$Transaction")
+        .bean(demo.camel.TransactionProtos.Transaction.class, "parseFrom(${body})")
+        .toD("kafka:webtrans?key=${header.sender}"+registryconfig)
+        .setBody(simple("Transaction from ${header.sender} sent."))
         ;
 
     }
-
+    
+    
+    
 
     
 }
