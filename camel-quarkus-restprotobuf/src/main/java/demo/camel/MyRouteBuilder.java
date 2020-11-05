d7f775fa90f69a9182dfcc36d63d67ff78c50232
weimeilin79 <weimielin@gmail.com>
2020-11-05 10:36:01 -0500

    Update Quarkus Rest to Protobuf app

--- camel-quarkus-restprotobuf/src/main/java/demo/camel/MyRouteBuilder.java
+++ camel-quarkus-restprotobuf/src/main/java/demo/camel/MyRouteBuilder.java
@@ -4,7 +4,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.protobuf.ProtobufDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.apache.camel.model.rest.RestBindingMode;
-import java.util.LinkedHashMap;
+import java.io.InputStream;
 
 /**
  * A Camel Java DSL Router
@@ -14,18 +14,17 @@ public class MyRouteBuilder extends RouteBuilder {
     
 
     public void configure() {
-        //restConfiguration().component("netty-http").host("localhost").port("8080").bindingMode(RestBindingMode.auto);
         
-        //rest("/").post("transfer").consumes("application/json").to("direct:transfer");
-        
-        from("platform-http:/transfer?httpMethodRestrict=POST&consumes=application/json")
-        
-                .setHeader("receiverid", jsonpath("$.receiverid"))
-                .setHeader("sender", jsonpath("$.sender.userid"))
-                
-                .marshal().protobuf("demo.camel.TransactionProtos$Transaction")
-                .toD("kafka:webtrans?brokers=localhost:9092&groupId=producergroup&serializerClass=org.apache.kafka.common.serialization.ByteBufferSerializer&key=${header.sender}")
+//&serializerClass=org.apache.kafka.common.serialization.ByteBufferSerializer
 
+        from("netty-http:http://0.0.0.0:8081/transfer?CamelHttpMethod=POST&Content-Type=application/json")   
+        .setHeader("sender",jsonpath("$.sender.userid"))
+        .marshal().protobuf("demo.camel.TransactionProtos$Transaction")
+        //.bean(demo.camel.TransactionProtos.Transaction.class, "parseFrom(${body})")
+        .log("Sender: ${header.sender}")
+        //.log("Sender: ${body.transactionid}")
+        .toD("kafka:webtrans-quarkus?brokers=localhost:9092&key=${header.sender}&serializerClass=org.apache.kafka.common.serialization.ByteArraySerializer")
+        .setBody(simple("Transaction from ${header.sender} sent."))
         ;
 
     }
